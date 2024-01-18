fd109839544cf64906dd1ef611e4e22b90825450
Samin Taheri <58706708+samin-taheri@users.noreply.github.com>
2024-01-18 16:36:16 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Header from '@/components/Header';
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact me */}
+
+    </div>
+  )
+}
+export default Home;
