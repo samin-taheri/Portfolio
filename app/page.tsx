58a41717d5cb7500c773bd71ee856035b6298d2c
Samin Taheri <58706708+samin-taheri@users.noreply.github.com>
2024-01-19 16:32:12 +0300

    commiting changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
 
       {/* Header */}
       <Header />
-      {/* Hero */}
-
+      {/* Hero Banner */}
+      <section id="hero" className='snap-center'>
+        <Hero />
+      </section>
       {/* About */}
 
       {/* Experience */}
---
+++ components/Hero.tsx
@@ -0,0 +1,35 @@
+'use client';
+import React from 'react';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
+import Link from 'next/link';
+
+type Props = {};
+
+export default function Hero({ }: Props) {
+    const [text, count] = useTypewriter({
+        words: ["Hi, My Name is Samin", "<ILoveToCode/>"],
+        loop: true,
+        delaySpeed: 2000, // Adjust the delay speed to make the typing animation smoother
+    });
+
+    return (
+        <div className='h-screen flex flex-col items-center justify-center align-middle text-center overflow-hidden relative'>
+            <BackgroundCircles />
+            <img src="/samin.jpeg" alt="bg-29" className="rounded-full h-32 w-32 mx-auto object-cover" />
+            <div className='z-20'>
+                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Frontend Developer</h2>
+                <h1 className='text-4xl lg: 6xl font-semibold  scroll-px-10'>
+                    <span className='mr-3'>{text}</span>
+                    <Cursor cursorColor='#8336c8' />
+                </h1>
+                <div className='pt-5'>
+                    <Link href={'#about'}><button className='heroButton'>About</button></Link>
+                    <Link href={'#experience'}><button className='heroButton'>Experience</button></Link>
+                    <Link href={'#skills'}><button className='heroButton'>Skills</button></Link>
+                    <Link href={'#projects'}><button className='heroButton'>Projects</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+}
